644c8a6e6ab4c8ec54f9626ef222927d734aae3d
Eamon <1025942836@qq.com>
2024-08-30 11:22:20 +0800

    chore: Remove invalid code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,8 @@
 "use client"
 
-import {useCallback, useMemo} from "react";
-import {Button} from "antd";
 import Image from "next/image";
-import ConnectWallet from "@/components/ConnectWallet";
-import {useAccount} from "wagmi";
-import {Avatar,DoubleAvatar} from "@/components/Avatar";
-import {getWalletIcon, shortenAddress} from "@/utils";
-import {useConnectWalletMenuStore} from "@/store/connectWalletMenu";
-import { WagmiConnectWallet } from "../ConnectWalletMenu";
 
 const Header = () => {
-  const {visible, setVisible} = useConnectWalletMenuStore()
-  const {isConnected} = useAccount();
-  const {address, connector} = useAccount();
-  const formattedAddress = useMemo(() => shortenAddress(address), [address]);
 
   return <header className="relative p-4 flex justify-between items-center z-10">
     {/* Logo */}
@@ -26,20 +14,8 @@ const Header = () => {
     </div>
     {/* Settings Button */}
     <div className="flex items-center space-x-4 z-20">
-      {isConnected && <div className="bg-white rounded-full px-3 py-1 flex items-center gap-2 cursor-pointer hover:bg-gray-100 transition-colors"
-                           onClick={() => setVisible(!visible)}>
-        <DoubleAvatar mainImg={{url:getWalletIcon(connector),name:connector?.name}}/>
-        <span>{formattedAddress}</span>
-      </div> || <Button type="primary" onClick={() => setVisible(true)}>连接钱包</Button>}
+      <span>菜单</span>
     </div>
-    {/* <div className={`w-full fixed bottom-0 left-0 right-0 transition-transform rounded-t-lg
-    md:w-[288px] md:absolute md:top-20 md:right-3.5 md:bottom-auto md:left-auto
-    md:rounded-lg bg-white p-3 z-50 ${visible ? "translate-y-0 md:block" : "translate-y-[200%] md:hidden"}`}>
-      <ConnectWallet/>
-    </div>
-    {visible &&
-      <div className="fixed top-0 left-0 w-full h-full md:bg-transparent bg-black opacity-50" onClick={() => setVisible(false)}></div>} */}
-      <WagmiConnectWallet/>
   </header>
 }
 export default Header
