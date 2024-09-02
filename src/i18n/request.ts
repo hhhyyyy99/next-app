687f79e16eb048453b5db56abd9f1817b2a9b3af
Eamon <1025942836@qq.com>
2024-09-02 11:32:36 +0800

    add i18n config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,10 @@
 import {getRequestConfig} from 'next-intl/server';
+import {defaultLocale} from "@/i18n/config";
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = 'en';
+  const locale = defaultLocale;
 
   return {
     locale,
