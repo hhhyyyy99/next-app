6ef87bc8ca1146677d21be17525f9b37a8bca674
Eamon <1025942836@qq.com>
2024-09-03 18:07:17 +0800

    fix: 修改样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function Home() {
   return <AppLayout>
-   <div className="flex flex-col items-center gap-4 bg-white dark:bg-black">
-   <header className="dark:text-red-500">主页</header>
+   <div className="flex flex-col items-center gap-4 rounded-lg p-6 bg-gray-300 dark:bg-purple-500">
+   <header className="">主页</header>
     <Link href="/about" className="">关于我们</Link>
     <Link href="/profile" className="">资料</Link>
    </div>
